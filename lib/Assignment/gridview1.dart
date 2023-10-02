41c5666ec953ae7b6bded557aa6624525c6d98f3
Sona V Sajan <sona.v.sajan@gmail.com>
2023-10-02 15:40:11 +0530

    rm -rf Sona-V-Sajan

---
+++ lib/Assignment/gridview1.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: GridView1(),));
+}
+
+class GridView1 extends StatelessWidget {
+  var image = [
+    "assets/img/USA.jpg",
+    "assets/img/RUSSIA.jpg",
+    "assets/img/FRANCE.jpg",
+    "assets/img/ENGLAND.jpg",
+    "assets/img/CANADA.jpg"
+  ];
+  var text = ["USA", "LONDON", "ENGLAND", "RUSSIA", "CANADA"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(
+      title: Text("Gridview"),),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(5, (index) => Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                width: 400,
+                height: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  image: DecorationImage
+                    (fit: BoxFit.fill,
+                    image:  AssetImage(
+                    image[index],
+                  ),)
+                ),
+                
+              ),
+            ),
+            Positioned(
+              bottom: 40, // Adjust the top position as needed
+              left: 40,
+              child: Text(
+                text[index],
+                style: TextStyle(
+                  color: Colors.white, // Text color
+                  fontSize: 25, // Text size
+                  fontWeight: FontWeight.bold, // Text weight
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
